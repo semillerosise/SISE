94e1603752eb4f5dc48d833362ef46e22ac92e4f
semillerosise <semillerosise@unicauca.edu.co>
2025-04-14 21:00:12 -0500

    Test 5: Routes

--- assets/js/dataTeam.js
+++ assets/js/dataTeam.js
@@ -23,7 +23,7 @@ const data = {
         "semilleros": "Sistemas Empotrados"
     },
     "user1": {
-        "photo": "../img/organizacion/alejandra.jpg",
+        "photo": "../../../SISE/assets/img/organizacion/alejandra.jpg",
         "name": "Maria Alejandra Casas Castillo",
         "cargo": "Coordinadora"
     },
